7f77412035c9dc82a5530827499572d5115d978b
Bilal Akmal <bilalakmal@hotmail.com>
2020-06-05 20:44:02 +0500

    threshold value 0.25 for now

--- unc_clustering.py
+++ unc_clustering.py
@@ -10,7 +10,7 @@ from typing import List
 from unc_document import Document
 
 
-def get_similar_documents(query_doc_id: int, documents: dict, threshold: float = 0.5) -> List[int]:
+def get_similar_documents(query_doc_id: int, documents: dict, threshold: float = 0.25) -> List[int]:
     """
     Returns list of documents similar to the given document.
     Documents with similarity below `threshold` will not be included.
@@ -24,14 +24,14 @@ def get_similar_documents(query_doc_id: int, documents: dict, threshold: float =
     similar_doc_ids: List[int] = list()
 
     for doc_id, document in documents.items():
-        jackard_index: float = get_jackard_index(query_document, document)
+        jackard_index: float = _get_jackard_index(query_document, document)
         if jackard_index >= threshold:
             similar_doc_ids.append(doc_id)
 
     return similar_doc_ids
 
 
-def get_jackard_index(document1: Document, document2: Document) -> float:
+def _get_jackard_index(document1: Document, document2: Document) -> float:
     # ji = (d1 intersection d2) / (d1 union d2)
     common: set = document1.features.intersection(document2.features)
     union: set = document1.features.union(document2.features)
