c65f05fe14c76bfedfcafc7c967118e2dbafa3fb
Bilal Akmal <bilalakmal@hotmail.com>
2020-06-05 20:33:17 +0500

    Better formatting

--- unc_preprocessing.py
+++ unc_preprocessing.py
@@ -1,29 +1,31 @@
-def extract_features(text: str, stopwords=None):
+"""
+This file defines the preprocessing method.
+For now, it removes punctuation and stopwords.
+
+
+(C) 2020 Urdu News Clustering
+"""
+from typing import Optional
+
+
+def extract_features(text: str, stopwords: Optional[set]) -> set:
     """
     Returns set of terms from a string.
 
-    :type text: str
-    :type stopwords: set
     :param stopwords: set of stop words
     :param text: original string
     :return: set of tokens after removing punctuation and stop words.
     """
-    if stopwords is None:
-        stopwords = set()
-    string: str = text\
-        .replace('\'', ' ')\
-        .replace('\"', ' ')\
-        .replace('.', ' ')\
-        .replace(',', '')\
-        .replace('،', ' ')\
-        .replace('!', ' ')\
-        .replace('؟', ' ')\
-        .replace('’', ' ')\
-        .replace('۔', ' ')\
-        .replace('‘', ' ')
+    punctuation_marks = ['\'', '\"', '.', '،', ',', '!', '؟', '’', '۔', '‘']
 
-    terms = set(string.split())
-    if stopwords:
+    # remove punctuation marks
+    for pm in punctuation_marks:
+        if pm in text:
+            text = text.replace(pm, ' ')
+
+    terms = set(text.split())  # split into tokens
+
+    if stopwords:  # remove stopwords
         terms.difference_update(stopwords)
 
     return terms
