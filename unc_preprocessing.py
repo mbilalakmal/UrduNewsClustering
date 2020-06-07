d4f82ae1b02faa23a7b09f2cf8a5516361bf6a07
Bilal Akmal <bilalakmal@hotmail.com>
2020-06-07 06:40:28 +0500

    Added more punctuation marks and a truncate parameter

--- unc_preprocessing.py
+++ unc_preprocessing.py
@@ -5,18 +5,19 @@ For now, it removes punctuation and stopwords.
 
 (C) 2020 Urdu News Clustering
 """
-from typing import Optional
+from typing import Optional, Set
 
 
-def extract_features(text: str, stopwords: Optional[set]) -> set:
+def extract_features(text: str, stopwords: Optional[Set[str]], truncate: int = 7):
     """
     Returns set of terms from a string.
 
+    :param truncate: terms longer than this will be truncated to this length
     :param stopwords: set of stop words
     :param text: original string
     :return: set of tokens after removing punctuation and stop words.
     """
-    punctuation_marks = ['\'', '\"', '.', '،', ',', '!', '؟', '’', '۔', '‘']
+    punctuation_marks = ['\'', '\"', '.', '،', ',', '!', '؟', '’', '۔', '‘', '-', 'ء', '؛']
 
     # remove punctuation marks
     for pm in punctuation_marks:
@@ -25,7 +26,14 @@ def extract_features(text: str, stopwords: Optional[set]) -> set:
 
     terms = set(text.split())  # split into tokens
 
-    if stopwords:  # remove stopwords
+    if stopwords is not None:  # remove stopwords
         terms.difference_update(stopwords)
 
-    return terms
+    trunc_terms: Set[str] = set()
+    for term in terms:
+        if len(term) > truncate:
+            trunc_terms.add(term[:truncate])
+        else:
+            trunc_terms.add(term)
+
+    return trunc_terms
