335e66de5fcb7351b9cb4c896dcdfa1f264c9085
Bilal Akmal <bilalakmal@hotmail.com>
2020-06-07 06:40:00 +0500

    Changed dict of Documents to List of Documents

--- unc_filing.py
+++ unc_filing.py
@@ -7,8 +7,10 @@ and traversing files and folders.
 """
 
 import os
-from unc_preprocessing import extract_features
+from typing import Set, Optional, List
+
 from unc_document import Document
+from unc_preprocessing import extract_features
 
 
 def read_stopwords(file_path: str):
@@ -17,57 +19,46 @@ def read_stopwords(file_path: str):
     return stopwords
 
 
-def read_headlines(root_path: str, stopwords: set):
+def read_headlines(root_path: str, stopwords: Optional[Set[str]]):
     """
     Reads headlines from `root_path` and returns a dict.
     Folders one level below are considered the source labels.
     Folders two levels below are considered the category labels.
     .doc files three levels below are considered as headlines.
 
-    :param stopwords: set of stop words
+    :param stopwords: Set of stop words
     :param root_path: Folder directory of dataset
-    :return: Dictionary containing the headlines
+    :return: List of Documents
     """
-    documents = []
+    documents: List[Document] = []
 
+    # get source names [bbc dataset etc]
     source_folders = next(os.walk(root_path))[1]
 
     for source_folder in source_folders:
+        # get category label [sports etc]
         category_folders = next(os.walk(os.path.join(root_path, source_folder)))[1]
 
         for category_folder in category_folders:
+            # get individual headlines
             headlines = next(
                 os.walk(
                     os.path.join(
-                        os.path.join(root_path, source_folder),
-                        category_folder
+                        os.path.join(root_path, source_folder), category_folder
                     )
                 )
             )[2]
+
             # remove file extensions (something.doc -> something)
             headlines = [headline[:headline.rfind(".")] for headline in headlines]
 
+            # Create document object from each headline
             for headline in headlines:
                 features = extract_features(headline, stopwords)
-                documents.append(Document(source_folder, category_folder, headline, features))
+                document = Document(
+                    text=headline, features=features,
+                    source=source_folder, category=category_folder
+                )
+                documents.append(document)
 
-    documents = {idx: document for idx, document in enumerate(documents)}
     return documents
-
-
-if __name__ == '__main__':
-    stopwords = read_stopwords(r'stopwords.txt')
-    documents = read_headlines(r'dataset', stopwords)
-
-    # for document in documents.values():
-    #     print(document)
-
-    from unc_clustering import get_similar_documents
-    import random
-    random = random.choice(range(len(documents)))
-    document = documents[random]
-
-    similar_documents = get_similar_documents(random, documents)
-    for doc in similar_documents:
-        print(documents[doc])
-
