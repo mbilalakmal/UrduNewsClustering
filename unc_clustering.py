1b894f7dda6aac12d3fff04dd58c5b40dbb6474d
Bilal Akmal <bilalakmal@hotmail.com>
2020-06-07 06:39:05 +0500

    Better formatting

--- unc_clustering.py
+++ unc_clustering.py
@@ -10,29 +10,33 @@ from typing import List
 from unc_document import Document
 
 
-def get_similar_documents(query_doc_id: int, documents: dict, threshold: float = 0.25) -> List[int]:
+def get_similar_documents(
+        query_document: Document,
+        documents: List[Document],
+        threshold: float = 0.20
+):
     """
     Returns list of documents similar to the given document.
     Documents with similarity below `threshold` will not be included.
-
-    :param threshold: lower bound on similarity index
-    :param documents: The collection of documents to search from.
-    :param query_doc_id: The original document selected by the user.
-    :return: list of documents similar to the query document.
     """
-    query_document: Document = documents[query_doc_id]
-    similar_doc_ids: List[int] = list()
+    similar_documents: List[Document] = list()
 
-    for doc_id, document in documents.items():
+    for document in documents:
         jackard_index: float = _get_jackard_index(query_document, document)
         if jackard_index >= threshold:
-            similar_doc_ids.append(doc_id)
+            similar_documents.append(document)
 
-    return similar_doc_ids
+    return similar_documents
 
 
-def _get_jackard_index(document1: Document, document2: Document) -> float:
-    # ji = (d1 intersection d2) / (d1 union d2)
+def _get_jackard_index(document1: Document, document2: Document):
+    """
+    Calculates and returns the jackard index between two documents
+    represented as sets of features.
+    :param document1: First Document
+    :param document2: Second Document
+    :return: jackard index value defined as intersection/union
+    """
     common: set = document1.features.intersection(document2.features)
     union: set = document1.features.union(document2.features)
     ji: float = len(common) / len(union)
