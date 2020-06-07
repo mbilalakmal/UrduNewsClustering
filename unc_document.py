75a0c10f61ea8b92931eeb996d5c34a3b170d58d
Bilal Akmal <bilalakmal@hotmail.com>
2020-06-07 06:39:16 +0500

    Better formatting

--- unc_document.py
+++ unc_document.py
@@ -6,16 +6,23 @@ of an actual text document.
 
 (C) 2020 Urdu News Clustering
 """
+from typing import Set
 
 
 class Document:
     def __init__(
             self,
             text: str,
-            features: str,
+            features: Set[str],
             source: str = 'User',
             category: str = 'User',
     ):
+        """
+        :param text: sequence of characters forming the news headline
+        :param features: set of terms after stopwords and punctuation is removed
+        :param source: news source for the article
+        :param category: category for the article
+        """
         self.source = source
         self.category = category
         self.text = text
